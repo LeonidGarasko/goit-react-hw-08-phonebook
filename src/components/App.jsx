ef6ebd30903ddb675f47841a541dab0a5f7fd780
LeonidGarasko <djjiehuh@gmail.com>
2022-08-14 13:20:13 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,18 @@
-export const App = () => {
-  return (
-    <div></div>
-  );
-};
+import { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+      </>
+    );
+  }
+}
