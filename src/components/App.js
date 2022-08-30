b62f4514d67abee2799cb66ba6b4f297df75b07e
LeonidGarasko <djjiehuh@gmail.com>
2022-08-30 19:36:27 +0300

    upd

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,19 @@ export class App extends Component {
 
     filter: '',
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContats = JSON.parse(contacts);
+
+    if (parsedContats) {
+      this.setState({ contacts: parsedContats });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleChange = e => {
     const { name, value } = e.currentTarget;
