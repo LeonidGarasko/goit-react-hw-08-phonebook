9f4eb474c89d2ba168187cf1cdd20f49931d9109
LeonidGarasko <djjiehuh@gmail.com>
2022-09-04 15:04:17 +0300

    upd

--- src/components/App.js
+++ src/components/App.js
@@ -1,89 +1,57 @@
 import { Phonebook } from './Phonebook/Phonebook';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid/non-secure';
 import { Main } from './Main/Main';
 import { Contacts } from './Phonebook/Contacts/Contacts';
 
-export class App extends Component {
-  state = {
-    contacts: [],
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const localStorageContacts = window.localStorage.getItem('contacts');
+    const parsedContats = JSON.parse(localStorageContacts);
+    return parsedContats ?? [];
+  });
+  const [filter, setFilter] = useState('');
 
-    filter: '',
-  };
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContats = JSON.parse(contacts);
-
-    if (parsedContats) {
-      this.setState({ contacts: parsedContats });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({
-      [name]: value,
-    });
+  const handleChange = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  formSubmitHandle = data => {
+  const formSubmitHandle = data => {
     const id = nanoid();
-    if (
-      this.state.contacts.filter(contact => contact.name === data.name).length >
-      0
-    ) {
+    if (contacts.filter(contact => contact.name === data.name).length > 0) {
       alert(`${data.name} is already in contacts`);
       return;
     }
-    this.setState({
-      contacts: [
-        ...this.state.contacts,
-        {
-          name: data.name,
-          number: data.number,
-          id: id,
-        },
-      ],
+    setContacts(prevState => {
+      return [...prevState, { name: data.name, number: data.number, id: id }];
     });
   };
 
-  onClickDelete = id => {
-    this.setState({
-      contacts: this.state.contacts.filter(contact => contact.id !== id),
-    });
+  const onClickDelete = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
   };
+  const normalizeFilter = filter.toLowerCase();
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizeFilter)
+  );
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  render() {
-    const normolizeFilter = this.state.filter.toLowerCase();
-    const visibleContacts = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normolizeFilter)
-    );
-    return (
-      <Main title="Phonebook">
-        <Phonebook
-          onChange={this.handleChange}
-          onSubmit={this.formSubmitHandle}
+  return (
+    <Main title="Phonebook">
+      <Phonebook onSubmit={formSubmitHandle} />
+      {contacts.length > 0 ? (
+        <Contacts
+          name="Contacts"
           contactsList={visibleContacts}
-          notEmptyList={this.state.contacts.length}
-          valueFilter={this.state.filter}
+          onChange={handleChange}
+          value={filter}
+          onClickDelete={onClickDelete}
         />
-        {this.state.contacts.length > 0 ? (
-          <Contacts
-            name="Contacts"
-            contactsList={visibleContacts}
-            onChange={this.handleChange}
-            value={this.state.filter}
-            onClickDelete={this.onClickDelete}
-          />
-        ) : (
-          <p>Phonebook empty</p>
-        )}
-      </Main>
-    );
-  }
-}
+      ) : (
+        <p>Phonebook empty</p>
+      )}
+    </Main>
+  );
+};
--- src/components/Phonebook/Phonebook.js
+++ src/components/Phonebook/Phonebook.js
@@ -1,49 +1,90 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { InputName } from './Input/InputName/InputName';
 import { LabelPhoneBook } from './Label/Label';
 import { ButtonSubmit } from './Button/ButtonSubmit';
 import { InputNumber } from './Input/InputNumber/inputNumber';
 import { FormPhonebook } from './Form/Form';
 
-export class Phonebook extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export const Phonebook = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({
-      [name]: value,
-    });
+  const handleChange = e => {
+    switch (e.currentTarget.name) {
+      case 'name':
+        setName(e.currentTarget.value);
+        break;
+      case 'number':
+        setNumber(e.currentTarget.value);
+        break;
+      default:
+        return;
+    }
   };
-
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
-
-  clickOnBtnSubmit = e => {
+  const clickOnBtnSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name, number });
+    reset();
   };
 
-  render() {
-    return (
-      <>
-        <FormPhonebook onSubmit={this.clickOnBtnSubmit}>
-          <LabelPhoneBook title="Name">
-            <InputName value={this.state.name} onChange={this.handleChange} />
-          </LabelPhoneBook>
-          <LabelPhoneBook title="Number">
-            <InputNumber
-              value={this.state.number}
-              onChange={this.handleChange}
-            />
-          </LabelPhoneBook>
-          <ButtonSubmit text="Add contact" />
-        </FormPhonebook>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <FormPhonebook onSubmit={clickOnBtnSubmit}>
+        <LabelPhoneBook title="Name">
+          <InputName value={name} onChange={handleChange} />
+        </LabelPhoneBook>
+        <LabelPhoneBook title="Number">
+          <InputNumber value={number} onChange={handleChange} />
+        </LabelPhoneBook>
+        <ButtonSubmit text="Add contact" />
+      </FormPhonebook>
+    </>
+  );
+};
+
+// export class oldPhonebook extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   handleChange = e => {
+//     const { name, value } = e.currentTarget;
+//     this.setState({
+//       [name]: value,
+//     });
+//   };
+
+//   reset = () => {
+//     this.setState({ name: '', number: '' });
+//   };
+
+//   clickOnBtnSubmit = e => {
+//     e.preventDefault();
+//     this.props.onSubmit(this.state);
+//     this.reset();
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <FormPhonebook onSubmit={this.clickOnBtnSubmit}>
+//           <LabelPhoneBook title="Name">
+//             <InputName value={this.state.name} onChange={this.handleChange} />
+//           </LabelPhoneBook>
+//           <LabelPhoneBook title="Number">
+//             <InputNumber
+//               value={this.state.number}
+//               onChange={this.handleChange}
+//             />
+//           </LabelPhoneBook>
+//           <ButtonSubmit text="Add contact" />
+//         </FormPhonebook>
+//       </>
+//     );
+//   }
+// }
